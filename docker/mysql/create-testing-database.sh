9842edf07cbca12315cc39ef26c5280605ef259d
Manabu Matsui <manabu.matsui@gmail.com>
2022-05-29 09:44:39 +0900

    sailへの依存を減らす

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS testing;
+    GRANT ALL PRIVILEGES ON testing.* TO '$MYSQL_USER'@'%';
     CREATE DATABASE IF NOT EXISTS testing_a1;
     GRANT ALL PRIVILEGES ON testing_a1.* TO '$MYSQL_USER'@'%';
     CREATE DATABASE IF NOT EXISTS testing_a2;
